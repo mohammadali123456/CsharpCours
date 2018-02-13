77ac9e67802155e4cfa08accbb2204a57f6b7ec3
CEC <CEC@DESKTOP-5I8JDAV>
2018-02-13 22:46:07 +0200

    Add project files.

---
+++ Part_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable 
+            int my_Value = 10;
+            Console.WriteLine("{0}" ,my_Value);
+
+            Console.WriteLine("ahmad ali");
+            Console.ReadKey();
+        }
+    }
+}
